6aaa594f2c36caca5a0cc1455ffb66e022e400f3
Ryan Canfield <rcanfiel@uw.edu>
2021-03-24 13:46:14 -0700

    Ryan's github practice

--- aopy/tutorial_functions.py
+++ aopy/tutorial_functions.py
@@ -4,5 +4,17 @@ This is an example module to practice using git
 
 # Ryan's practice function
 
-def practice_func_ryan():
-    print('Hello Ryan')
+def practice_func_ryan(user_name):
+    '''
+        This function returns a print statement specific to Ryan from the input
+        username
+
+        Inputs:
+            user_name [string]: Name of person running the function
+        
+        Outputs:
+            print statement
+    '''
+    print('Hello Ryan, from', user_name)
+
+
