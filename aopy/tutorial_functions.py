6723760be0f843039af18e9e54f7b7ce58f9c76e
Ryan Canfield <rcanfiel@uw.edu>
2021-03-25 16:03:30 -0700

    Ryan's github practice: Added function practice_func_ryan2 to tutorial_functions.py

--- aopy/tutorial_functions.py
+++ aopy/tutorial_functions.py
@@ -11,9 +11,21 @@ def practice_func_ryan(user_name):
         user_name [string]: Name of person running the function
         
     Outputs:
-        print statement
+        string_out [string]: Hello Ryan from 
     '''
-    
-    print('Hello Ryan, from', user_name)
+    string_out = 'Hello Ryan, from' + user_name
+    return string_out
 
+# Ryan's practice function 2
+def practice_func_ryan2(favorite_food):
+    '''
+    This function returns a print statement specific to Ryan from the input username
 
+    Inputs:
+        favorite_food [string]: Your favorite food
+        
+    Outputs:
+        None; print statement
+    '''
+    string_out = 'My favorite food is' + favorite_food
+    return string_out
